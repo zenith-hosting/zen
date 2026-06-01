3c0ec332aeeca5d7b35ff4bc8f7860adc433d9ce
Logykk <logykk@logykk.stream>
2026-06-01 17:37:36 +1200

    feat: add fiber static asset helper

---
+++ zen/static.go
@@ -0,0 +1,12 @@
+package zen
+
+import (
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/static"
+)
+
+func (r *Renderer) Static() fiber.Handler {
+	return static.New(filepath.Join(r.config.ClientDist, "assets"))
+}
---
+++ zen/static_test.go
@@ -0,0 +1,43 @@
+package zen
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/zenith/zen/internal/testutil"
+)
+
+func TestStaticReturnsFiberHandlerForClientDist(t *testing.T) {
+	dir := t.TempDir()
+	assetsDir := filepath.Join(dir, "assets")
+
+	if err := os.MkdirAll(assetsDir, 0o755); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := os.WriteFile(filepath.Join(assetsDir, "app.js"), []byte("console.log('ok')"), 0o644); err != nil {
+		t.Fatal(err)
+	}
+
+	r := &Renderer{
+		config: Config{
+			ClientDist: dir,
+		},
+	}
+
+	app := fiber.New()
+	app.Get("/assets/*", r.Static())
+
+	res := testutil.PerformRequest(t, app, http.MethodGet, "/assets/app.js", "")
+	body := testutil.ReadBody(t, res)
+
+	if res.StatusCode != http.StatusOK {
+		t.Fatalf("expected status 200, got %d", res.StatusCode)
+	}
+	if body != "console.log('ok')" {
+		t.Fatalf("unexpected body: %s", body)
+	}
+}
